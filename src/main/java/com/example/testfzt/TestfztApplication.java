bddae13a259b69f90549ea2012f20c836fbe3476
jym-dev <jym_dev@163.com>
2022-08-03 18:20:12 +0800

    Initial commit

---
+++ src/main/java/com/example/testfzt/TestfztApplication.java
@@ -0,0 +1,14 @@
+package com.example.testfzt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfztApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestfztApplication.class, args);
+    }
+
+    //
+}
---
+++ src/test/java/com/example/testfzt/TestfztApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfzt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfztApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
